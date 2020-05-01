d9f7eeb8be639ca04540514e6fb921f00210e073
Fareed Uddin <md.fareed316@gmail.com>
2020-05-01 04:38:38 -0400

    Added Automating_getting_SandP500

---
+++ Automating_getting_SandP500.py
@@ -0,0 +1,23 @@
+import bs4 as bs
+import pickle 
+import requests
+
+# pickle serializes any python object 
+
+def save_sp500_tickers():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text,'lxml')
+    table = soup.find('table', {'id': 'constituents'})
+    tickers = []
+    
+    for row in table.findAll('tr')[1:]:
+        ticker = row.find('td').text.strip()
+        tickers.append(ticker)
+    
+    with open('sp500tickers.pickle','wb') as f:
+        pickle.dump(tickers,f)
+        
+    print(tickers)
+    
+    return tickers
+save_sp500_tickers()
